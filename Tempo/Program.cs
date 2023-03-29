ed2fd5746829d20faee661fd9b235add4e3667d8
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-29 22:13:42 +0100

    Exercise 4 variables creation

---
+++ Tempo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declares And Initiates crono1 And crono2;
+        Stopwatch crono1 = new Stopwatch();
+        Stopwatch crono2 = new Stopwatch();
+
+        // Starts To Count The Time In crono1 And crono2;
+        crono1.Start();
+        crono2.Start();
+
+    }
+}
+
