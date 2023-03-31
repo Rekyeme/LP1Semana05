691f208ae80d6c589303816657f8637eb5b756ef
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-31 10:30:14 +0100

    Ex.5, reading the user input

---
+++ RandomDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of dices you wish to have: ");
+            int n = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
