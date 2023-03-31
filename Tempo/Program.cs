4b55b23bd98545d4d015c9e581db9b557d84f5d8
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-31 10:11:42 +0100

    Corrections and final version of ex.4

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,8 +12,18 @@ class Program
 
         // Starts To Count The Time In crono1 And crono2;
         crono1.Start();
-        crono2.Start();
+        Thread.Sleep(500);
 
+        crono2.Start();
+        Thread.Sleep(300);
+
+        crono1.Stop();
+        crono2.Stop();
+
+        double elapsedTime1 = crono1.Elapsed.TotalSeconds;
+        double elapsedTime2 = crono2.Elapsed.TotalSeconds;
+
+        Console.WriteLine($"Run time for each chronometer {elapsedTime1:F2} , {elapsedTime2:F2}");
     }
 }
 
