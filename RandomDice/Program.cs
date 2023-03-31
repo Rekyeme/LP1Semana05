b11bdc0e815691cebe73721886409ac905cc5a7b
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-31 10:42:16 +0100

    Ex.5, adition of a Random and sum of the numbers of it

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -9,6 +9,15 @@ namespace RandomDice
             Console.Write("Enter the number of dices you wish to have: ");
             int n = int.Parse(Console.ReadLine());
 
+            Random random = new Random();
+
+            Console.WriteLine($"Rolling {n} dices: ");
+            for (int i = 0; i < n; i++)
+            {
+                int result = random.Next(1, 7);
+                Console.Write(result + "");
+            }
+            Console.WriteLine();
         }
     }
 }
